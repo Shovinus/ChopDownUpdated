f359ed4b058b7f8e061a591960788c5ba4f2bcb3
Shovinus <baileyjoseph87@hotmail.com>
2018-11-05 20:39:01 +0000

    Switch to gradle overview

--- versions/1.9-1.11.2/src/main/java/com/shovinus/chopdownupdated/config/PersonalConfig.java
+++
@@ -1,7 +0,0 @@
-package com.shovinus.chopdownupdated.config;
-
-public class PersonalConfig {
-	public boolean makeGlass = false;
-	public boolean dontFell = false;
-	public boolean showBlockName = false;
-}
