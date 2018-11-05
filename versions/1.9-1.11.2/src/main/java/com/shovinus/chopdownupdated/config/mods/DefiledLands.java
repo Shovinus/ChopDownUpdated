73abe6345248141e4b98dc9c61db6dca607af435
Shovinus <baileyjoseph87@hotmail.com>
2018-11-05 21:10:15 +0000

    Split in to branches

--- versions/1.9-1.11.2/src/main/java/com/shovinus/chopdownupdated/config/mods/DefiledLands.java
+++
@@ -1,8 +0,0 @@
-package com.shovinus.chopdownupdated.config.mods;
-
-import com.shovinus.chopdownupdated.config.TreeConfiguration;
-
-public class DefiledLands {
-	public static TreeConfiguration[] Trees = new TreeConfiguration[] {
-			new TreeConfiguration().setLogs("defiledlands:tenebra_log:0").setLeaves("defiledlands:tenebra_leaves:0") };
-}
